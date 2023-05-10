42571105454a35e2c839cef90cf455f073e20948
jan <janofficial2002@gmail.com>
2023-05-10 10:27:54 +0200

    Initial commit

---
+++ src/test/java/pl/january/jbrowski/storeeverything/StoreEverythingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.january.jbrowski.storeeverything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreEverythingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
