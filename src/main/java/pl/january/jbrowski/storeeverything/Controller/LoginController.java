68d3b1b909b47283e1eaaec7ad49d668a4dde7da
janua <janofficial2002@gmail.com>
2023-05-23 23:38:34 +0200

    Did some repos, made database stored in file in resources with connection (h2 database), created some models and made login controller and service work. Able to get all users and info.

--- src/main/java/pl/january/jbrowski/storeeverything/Controller/LoginController.java
+++ src/main/java/pl/january/jbrowski/storeeverything/Controller/LoginController.java
@@ -1,10 +1,15 @@
 package pl.january.jbrowski.storeeverything.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.january.jbrowski.storeeverything.Service.LoginService;
 
-@RestController
+import java.util.Optional;
+
+@Controller
 public class LoginController {
     private final LoginService loginService;
 
@@ -13,7 +18,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(){
-        return loginService.login();
+    public String login(@RequestParam("username")String username, @RequestParam("password") String password, Model model){
+        return loginService.login(username, password, model);
     }
 }
